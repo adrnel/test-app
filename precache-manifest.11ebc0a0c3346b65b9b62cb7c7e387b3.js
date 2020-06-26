0ec02f9c882484544d6766fcbceed40381316d17
Adrian Nelson <anel1989@googlemail.com>
2020-06-26 09:31:29 +0100

    Updates

---
+++ precache-manifest.11ebc0a0c3346b65b9b62cb7c7e387b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab839143e831873361994dc39b7d098",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "225322d1d3efedf57d4e",
+    "url": "/test-app/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "9bc4e04b87fe88c4cc83",
+    "url": "/test-app/static/css/main.2d51ff4f.chunk.css"
+  },
+  {
+    "revision": "225322d1d3efedf57d4e",
+    "url": "/test-app/static/js/2.8ec80713.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-app/static/js/2.8ec80713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bc4e04b87fe88c4cc83",
+    "url": "/test-app/static/js/main.e136b013.chunk.js"
+  },
+  {
+    "revision": "010976abd684515c15d4",
+    "url": "/test-app/static/js/runtime-main.107bf183.js"
+  }
+]);
