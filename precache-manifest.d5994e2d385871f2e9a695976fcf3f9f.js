1ed3058197a58b509d3e9cd4a448e4d45463890e
adrnel <anel1989@googlemail.com>
2020-06-26 13:45:51 +0000

    Updates

--- precache-manifest.d5994e2d385871f2e9a695976fcf3f9f.js
+++ precache-manifest.d5994e2d385871f2e9a695976fcf3f9f.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5895de966f2787b27a29473748ad28ab",
+    "revision": "bceaad2d3b2f6d5384549426ee516009",
     "url": "/test-app/index.html"
   },
   {
-    "revision": "225322d1d3efedf57d4e",
+    "revision": "63af0300e98487a796fa",
     "url": "/test-app/static/css/2.3cb76441.chunk.css"
   },
   {
-    "revision": "0796b37c9235c8893474",
+    "revision": "df0129f01c43782064fc",
     "url": "/test-app/static/css/main.2d51ff4f.chunk.css"
   },
   {
-    "revision": "225322d1d3efedf57d4e",
-    "url": "/test-app/static/js/2.8ec80713.chunk.js"
+    "revision": "63af0300e98487a796fa",
+    "url": "/test-app/static/js/2.bb9428ef.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/test-app/static/js/2.8ec80713.chunk.js.LICENSE.txt"
+    "url": "/test-app/static/js/2.bb9428ef.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0796b37c9235c8893474",
-    "url": "/test-app/static/js/main.4d5bbe0d.chunk.js"
+    "revision": "df0129f01c43782064fc",
+    "url": "/test-app/static/js/main.7779e181.chunk.js"
   },
   {
     "revision": "010976abd684515c15d4",
